e8874f6180ebbdccc4f8fa4e20514131db299790
Nethum Mihiranga <157717066+Nethum99@users.noreply.github.com>
2024-11-14 22:39:31 +1200

    Configuration in CLI

--- src/Configuration.java
+++ src/Configuration.java
@@ -2,9 +2,31 @@ import java.util.Scanner;
 
 public class Configuration {
 
-    private int totalTi
+    private int totalTickets;
+    private int maxCapacity;
+    private int ticketReleaseRate;
+    private int customerRetrievalRate;
+    private int activeCustomers;
+    private int activeVendors;
 
-    Scanner scanner = new Scanner(System.in);
+    public Configuration(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the total :");
+        this.totalTickets = scanner.nextInt();
+
+        System.out.println("Enter ticket release rate :");
+        this.ticketReleaseRate = scanner.nextInt();
+
+        System.out.println("Enter customer retrieval rate :");
+        this.customerRetrievalRate = scanner.nextInt();
+
+        System.out.println("Enter max capacity in ticketpool");
+        this.maxCapacity = scanner.nextInt();
+
+    }
+
+
     public void totalTickets(){
         System.out.println("Total number of ticket available for the event :");
     }
